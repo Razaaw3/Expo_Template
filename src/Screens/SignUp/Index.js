da1292ef2fffa20fc8b5d0250b0561ac80804361
Razaaw3 <razaaw3@gmail.com>
2023-08-31 13:43:18 +0500

    --js Expo Template

---
+++ src/Screens/SignUp/Index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Style from './Style'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  )
+}
+
+export default Index
